cca5d88217943c99633dde0c58d049d855ed3b22
davidsd29 <davidsdankwah@gmail.com>
2023-05-11 01:25:22 +0200

    rounding up

--- routers/pages.js
+++ routers/pages.js
@@ -2,80 +2,36 @@ import express from 'express';
 import { kledingKeuze } from '../data/storage.js';
 
 const form = express.Router();
-const queryString = (req) => req._parsedUrl.search;
-const searchParts = (req) => req.split('/');
-
-// import { index, end, done } from '../controllers/pages.js';
-
 form
-	.get('/', (req, res) => {res.render('pages/index');})
-    
-    .get('/style', (req, res) => {
-        const searchArray = searchParts(req._parsedOriginalUrl.pathname);
-		let values =
-			kledingKeuze.find((object) => {
-				if (
-					object.style == searchArray[1] &&
-					object.pageType == searchArray[0]
-				)
-					return object;
-			}) || {};
+	.get('/', (req, res) => {
+		res.render('pages/index');
+	})
 
+	.get('/style', (req, res) => {
 		const pageType = 'start';
 		res.render('pages/form', {
 			pageType,
-			values,
 		});
 	})
 
 	.get('/type', (req, res) => {
-		const searchArray = searchParts(req._parsedOriginalUrl.pathname);
-		let values =
-			kledingKeuze.find((object) => {
-				if (
-					object.top == searchArray[1] &&
-					object.pageType == searchArray[0]
-				)
-					return object;
-			}) || {};
 
-		const categorie = {
-			pageType: 'type',
-			style: req.query.style,
-		};
-
-		kledingKeuze.push(categorie);
+		kledingKeuze.push(req.query.style);
+		console.log(kledingKeuze);
 		const pageType = 'type';
 		res.render('pages/form', {
 			pageType,
-			values,
 		});
 	})
 
 	.get('/design', (req, res) => {
-		const searchArray = searchParts(req._parsedOriginalUrl.pathname);
-		let values =
-			kledingKeuze.find((object) => {
-				if (
-					object.topPattern == searchArray[1] &&
-					object.pageType == searchArray[0]
-				)
-					return object;
-			}) || {};
-
 		const pageType = 'design';
 		const onderkant = req.query.bottom;
 		const bovenkant = req.query.top;
 
-		const type = {
-			top: req.query.top,
-			bottom: req.query.bottom,
-		};
-
-		kledingKeuze.push(type);
-
+		kledingKeuze.push(req.query.top, req.query.bottom);
+		console.log(kledingKeuze);
 		res.render('pages/form', {
-            values,
 			pageType,
 			onderkant,
 			bovenkant,
@@ -83,28 +39,33 @@ form
 	})
 
 	.get('/kleur/', (req, res) => {
-        const searchArray = searchParts(req._parsedOriginalUrl.pathname);
-		let values =
-			kledingKeuze.find((object) => {
-				if (
-					object.topColor == searchArray[1] &&
-					object.pageType == searchArray[0]
-				)
-					return object;
-			}) || {};
-            
-		const pattern = {
-			topPattern: req.query.print,
-			bottomPattern: req.query.fabric,
-		};
 
-		kledingKeuze.push(pattern);
+		const hasPattern = (pattern) => {
+			if (pattern == 'plane') {
+				return 'geen printje';
+			} else {
+				return 'een printje';
+			}
+		};
+		console.log(kledingKeuze.style);
+
+		const keuze = `Je hebt gekozen voor de ${kledingKeuze[0]} style met een ${
+			kledingKeuze[1]
+		} en ${kledingKeuze[2]}. Je hebt gekozen voor een ${
+			kledingKeuze[1]
+		} met ${hasPattern(kledingKeuze[3])} en een ${kledingKeuze[4]} ${
+			kledingKeuze[2]
+		}`;
+
+		kledingKeuze.push(req.query.print, req.query.fabric);
 		console.log(kledingKeuze);
 
+		let bovenkant = kledingKeuze[1];
 		const pageType = 'kleur';
 		res.render('pages/form', {
+			keuze,
 			pageType,
-            values
+			bovenkant,
 		});
 	});
 
